fb2453ed238a4d844294c6ce37426ad98d7a8e64
Sahib <blackysh@protonmail.com>
2023-01-06 09:20:52 +0530

    c

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
+
+
 
 function App() {
   return (
     <div className="App">
-
+        <h1>hi</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,43 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+    <div>
+    <nav class="navbar navbar-dark bg-dark navbar-expand-lg" data-bs-theme="dark">
+    <div class="container-fluid">
+    <a class="navbar-brand" href="#">Website Name</a>
+    
+    
+    
+    
+    <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+        <li class="nav-item">
+        <button type="button" class="btn btn-dark ">Home</button>
+        </li>
+        <li class="nav-item">
+        <prep> </prep>
+        </li>
+        <li class="nav-item">
+        <button type="button" class="btn btn-dark">Downloads</button>
+        </li>
+
+      </ul>
+
+
+  
+
+      </div>
+    </div>
+
+      </nav>
     <App />
-  </React.StrictMode>
+    </div>
+    
+
 );
 
 // If you want to start measuring performance in your app, pass a function
