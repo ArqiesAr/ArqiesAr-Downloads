887d62878171084f5bc6f9c0fd825c8f476d3b2e
Sahib <blackysh@protonmail.com>
2023-01-07 12:47:58 +0530

    Fixed

--- src/pages/CARDS.jsx
+++ src/pages/CARDS.jsx
@@ -36,7 +36,7 @@ const Cards = () => {
     <h5 class="card-title">AFS</h5>
     <p class="card-text">Command line tool to install and remove apt/flatpak/snap packages from one place.</p>
     <li class="btn-group">
-    <a href="https://codeload.github.com/Blackysh/afs-apt-flatpak-snap/zip/refs/heads/master" class="btn btn-primary">Download</a>
+    <a href="https://objects.githubusercontent.com/github-production-release-asset-2e65be/585413689/16fe061e-967b-4715-8ff4-f578bebd5092?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20230107%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230107T071537Z&X-Amz-Expires=300&X-Amz-Signature=3b7a26a53422528bcb21d2f318990d6dd7bfbb28cdf209d7afbe6d931180f20b&X-Amz-SignedHeaders=host&actor_id=100060676&key_id=0&repo_id=585413689&response-content-disposition=attachment%3B%20filename%3Dafs&response-content-type=application%2Foctet-stream" class="btn btn-primary">Download</a>
     <a href="https://github.com/Blackysh/afs-apt-flatpak-snap" target="_blank" class="btn btn-primary">Github</a>
     </li>
   </div>
