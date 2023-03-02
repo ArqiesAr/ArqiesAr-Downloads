4371f85b845ca842503ab962ffa41da96d6bcb86
Sahib <blackysh@protonmail.com>
2023-03-02 18:39:08 +0530

    pppo2 a

--- src/pages/Freejc.jsx
+++ src/pages/Freejc.jsx
@@ -11,9 +11,8 @@ const Freejc = () => {
   //privacy policy paragraph under
 <div class="text-bg-light p-3">
 
-This app uses Google Admob, the data is collected by Google. Read their Privacy Policy here: (https://policies.google.com/privacy?hl=en-US).
-We do not process sensitive personal information ourselves.
-We do not receive any information from third parties.
+This app uses Google Admob, the for data collected by Google, read their Privacy Policy here: (https://policies.google.com/privacy?hl=en-US).
+No Other data is accessed by us or the application except Mobile Storage.
 Application Data. If you use our application(s), we also may collect the following information if you choose to provide us with access or permission:
 Mobile Device Access. We may request access or permission to certain features from your mobile device, including your mobile device's storage, and other features. If you wish to change our access or permissions, you may do so in your device's settings.
 
