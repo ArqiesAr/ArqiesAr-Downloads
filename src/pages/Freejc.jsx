543b82c191713ef4fb956fd95f05b7d67bf41d9d
Sahib <100060676+Blackysh@users.noreply.github.com>
2023-06-22 20:51:52 +0530

    asd

--- src/pages/Freejc.jsx
+++ src/pages/Freejc.jsx
@@ -12,7 +12,7 @@ const Freejc = () => {
 <div class="p-2 text-center d-flex flex-column align-items-center justify-content-center ">
 
 <p className="paragf fs-4 text-center  fst-italic" style={{maxWidth: '80%'}}> 
-Sahib's programming skills are absolutely outstanding! His expertise in coding are remarkable, and the ability to solve complex problems is truly impressive. He has deep understanding of different programming languages and frameworks which allows him to adapt to any project or challenge effortlessly.. His ability to think critically and logically sets him apart, allowing him to create innovative and groundbreaking solutions.
+Arqies's programming skills are absolutely outstanding! His expertise in coding are remarkable, and the ability to solve complex problems is truly impressive. He has deep understanding of different programming languages and frameworks which allows him to adapt to any project or challenge effortlessly.. His ability to think critically and logically sets him apart, allowing him to create innovative and groundbreaking solutions.
   </p><p className="hesaidthat fs-2 justify-self-right">~ Dark</p>
 
 </div>
